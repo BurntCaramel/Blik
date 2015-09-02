fe9911602a098a6409ccc6413f140cb4da998679
Patrick Smith <pgwsmith@gmail.com>
2015-09-02 21:47:31 +1000

    Add checkbox for controlling whether collected files are highlighted

--- Blik Mac/GLAFileCollectionViewController.m
+++ Blik Mac/GLAFileCollectionViewController.m
@@ -515,6 +515,7 @@
 	NSIndexSet *selectedIndexes = (sourceFilesListTableView.selectedRowIndexes);
 	
 	BOOL isContextual = ((sender != nil) && [sender isKindOfClass:[NSMenuItem class]]);
+	BOOL isControl = ((sender != nil) && [sender isKindOfClass:[NSControl class]]);
 	
 	if (isContextual) {
 		NSInteger clickedRow = (sourceFilesListTableView.clickedRow);
@@ -527,6 +528,15 @@
 		}
 		else {
 			return [NSIndexSet indexSetWithIndex:clickedRow];
+		}
+	}
+	else if (isControl) {
+		NSInteger row = [sourceFilesListTableView rowForView:(NSButton *)sender];
+		if (row == -1) {
+			return [NSIndexSet indexSet];
+		}
+		else {
+			return [NSIndexSet indexSetWithIndex:row];
 		}
 	}
 	else {
@@ -867,6 +877,16 @@
 	[pm editHighlightsOfProject:project usingBlock:editingBlock];
 }
 
+- (IBAction)toggleFileIsHighlighted:(NSButton *)sender
+{
+	if (sender.state == NSOnState) {
+		[self addSelectedFilesToHighlights:sender];
+	}
+	else {
+		[self removeSelectedFilesFromHighlights:sender];
+	}
+}
+
 - (IBAction)showShareMenuForSelectedFiles:(GLAButton *)sender
 {
 	NSArray *selectedURLs = (self.sourceListSelectedURLs);
@@ -1106,7 +1126,7 @@
 
 - (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
 {
-	NSTableCellView *cellView = nil;
+	FileCollectionTableCellView *cellView = nil;
 	
 #if EXTRA_ROW_COUNT == 1
 	if (row == 0) {
@@ -1123,12 +1143,14 @@
 #endif
 	
 	GLACollectedFile *collectedFile = [self collectedFileForRow:row];
+	GLAProjectManager *pm = (self.projectManager);
 	
 	cellView = [tableView makeViewWithIdentifier:@"collectedFile" owner:nil];
 	(cellView.objectValue) = collectedFile;
 	
 	NSString *displayName = nil;
 	NSImage *iconImage = nil;
+	BOOL isHighlighted = [pm collectedFilesAreAllHighlighted:@[collectedFile] fromCollectionWithUUID:(self.filesListCollection.UUID) projectUUID:(self.project.UUID)];
 	
 	if (collectedFile.empty) {
 		displayName = NSLocalizedString(@"(Gone)", @"Display name for empty collected file");
@@ -1142,6 +1164,7 @@
 	
 	(cellView.textField.stringValue) = displayName ?: @"Loading…";
 	(cellView.imageView.image) = iconImage;
+	(cellView.highlightsCheckbox.state) = isHighlighted ? NSOnState : NSOffState;
 	
 	return cellView;
 }
