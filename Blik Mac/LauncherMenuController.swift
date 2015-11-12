9cb499569e6f385fb23389126045495a1a8773a3
Patrick Smith <pgwsmith@gmail.com>
2015-11-12 20:49:52 +1100

    Update BurntCocoaUI

---
+++ Blik Mac/LauncherMenuController.swift
@@ -0,0 +1,9 @@
+//
+//  LauncherMenuController.swift
+//  Blik
+//
+//  Created by Patrick Smith on 12/11/2015.
+//  Copyright © 2015 Burnt Caramel. All rights reserved.
+//
+
+import Foundation
---
+++ Blik Mac/LauncherProjectMenuController.swift
@@ -0,0 +1,9 @@
+//
+//  LauncherProjectMenu.swift
+//  Blik
+//
+//  Created by Patrick Smith on 12/11/2015.
+//  Copyright © 2015 Burnt Caramel. All rights reserved.
+//
+
+import Foundation
