683837486c996652478aa31b5d30c05f2ed40733
Patrick Smith <pgwsmith@gmail.com>
2016-04-22 15:53:12 +1000

    Allow collection headings to work in launcher menu’s highlights

--- Blik Mac/GLAHighlightsTableCellView.m
+++ Blik Mac/GLAHighlightsTableCellView.m
@@ -11,27 +11,4 @@
 
 @implementation GLAHighlightsTableCellView
 
-- (void)mouseDown:(NSEvent *)theEvent
-{
-	NSMenuItem *menuItem = self.enclosingMenuItem;
-	if (menuItem) {
-		// Do not pass up to table view as normal
-	}
-	else {
-		[self.nextResponder mouseDown:theEvent];
-	}
-}
-
-- (void)mouseUp:(NSEvent *)theEvent
-{
-	NSMenuItem *menuItem = self.enclosingMenuItem;
-	if (menuItem) {
-		// Pass to performClick: handler, which GLATableView implements.
-		[self.nextResponder tryToPerform:@selector(performClick:) with:self];
-	}
-	else {
-		[self.nextResponder mouseUp:theEvent];
-	}
-}
-
 @end
--- Blik Mac/GLATableCellView.m
+++ Blik Mac/GLATableCellView.m
@@ -11,4 +11,32 @@
 
 @implementation GLATableCellView
 
+- (BOOL)acceptsFirstMouse:(NSEvent *)theEvent
+{
+	return YES;
+}
+
+- (void)mouseDown:(NSEvent *)theEvent
+{
+	NSMenuItem *menuItem = self.enclosingMenuItem;
+	if (menuItem) {
+		// Do not pass up to table view as normal, as we want to do the tracking
+	}
+	else {
+		[self.nextResponder mouseDown:theEvent];
+	}
+}
+
+- (void)mouseUp:(NSEvent *)theEvent
+{
+	NSMenuItem *menuItem = self.enclosingMenuItem;
+	if (menuItem) {
+		// Perform performClick:, which GLATableView implements.
+		[self.nextResponder tryToPerform:@selector(performClick:) with:self];
+	}
+	else {
+		[self.nextResponder mouseUp:theEvent];
+	}
+}
+
 @end
